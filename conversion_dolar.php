196c8815f57247801923c89fb6c8684c142f16aa
Raul-OXRI <jose.botzoc.30@gmail.com>
2024-04-01 15:57:35 -0600

    se hizo cambios de tamaño

--- conversion_dolar.php
+++ conversion_dolar.php
@@ -16,9 +16,9 @@ function get_tipo_conversion_dolar() {
     //de ancho  1px
     echo '
     <div style="margin: 8px 0 0; padding: 20px; text-align: center; ">
-  <div style="align-items: center; justify-content: space-between; padding: 7px; width: 115px; background-color: #ffffff;border-radius: 25px;">
-    <h2 style="font-size: 8px; margin: 0; font-family: Montserrat, sans-serif; color: #052a60">Conversión USD a GTQ</h2>
-    <span style="font-weight: bold; color: #052a60;font-size: 7px;">' . $tipo_texto . '</span>
+  <div style="align-items: center; justify-content: space-between; padding: 7px; width: 125px; background-color: #ffffff;border-radius: 25px;">
+    <h2 style="font-size: 10px; margin: 0; font-family: Montserrat, sans-serif; color: #052a60">Conversión USD a GTQ</h2>
+    <span style="font-weight: bold; color: #052a60;font-size: 9px;">' . $tipo_texto . '</span>
   </div>
 </div>
 
